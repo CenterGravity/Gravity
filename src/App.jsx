f5d09207bd80193f9a2ce14a97b9d735a2bdf96b
강준영 <j8521103@gmail.com>
2025-12-07 23:21:34 +0900

    나머지

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/Login'
 import { LoadingPage } from './components/LoadingPage'
 import { MainPage } from './components/MainPage'
 import { Theory } from './components/Theory'
+import { Simulation } from './components/Simulation'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/theory" element={<Theory />} />
+        <Route path="/simulation" element={<Simulation />} />
       </Routes>
     </Router>
   )
